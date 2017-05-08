a74444e46f2e41307b91382527c0c2356860ff15
Franklin Chieze <chieze.franklin@gmail.com>
2017-05-08 10:55:31 +0100

    Rebuilt for Heroku

--- errors.js
+++ errors.js
@@ -43,6 +43,7 @@ module.exports = {
 
 	'410': "App path missing",
 	'414': "Could not install app due to unsupported target version",
+	'415': "Could not download app from online repository",
 
 	'420': "App port missing",
 
